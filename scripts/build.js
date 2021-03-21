d896f3329e4548e02eb74becdadd34ea475029e1
William Lahti <wilahti@gmail.com>
2021-03-21 05:13:25 -0700

    convert to unix line endings on demand (ugh)

--- scripts/build.js
+++ scripts/build.js
@@ -2,6 +2,7 @@
 
 const child_process = require('child_process');
 const path = require('path');
+const fs = require('fs');
 
 if (process.platform === 'win32') {
     let proc = child_process.spawn(path.resolve(__dirname, 'build.bat'), { stdio: 'inherit' });
@@ -9,7 +10,14 @@ if (process.platform === 'win32') {
         console.error(`Error: ${err}`);
     });
 } else {
-    let proc = child_process.spawn(`bash ${path.resolve(__dirname, 'build.sh')}`, { stdio: 'inherit' });
+    let buildScript = path.resolve(__dirname, 'build.sh');
+    // i can't believe this is required ugh
+    // when publishing a package from Windows, the line endings will be CRLF
+    // and bash barfs on this...
+        
+    fs.writeFileSync(buildScript, fs.readFileSync(buildScript).toString('utf-8').replace(/\r\n/g, "\n"));
+
+    let proc = child_process.spawn(buildScript, { stdio: 'inherit' });
     proc.on('error', err => {
         console.error(`Error: ${err}`);
     });
