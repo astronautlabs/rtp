950b7dede54b48c286f493c9668d4a64eb052ab6
William Lahti <wilahti@gmail.com>
2021-03-21 14:29:13 -0700

    fix paths

--- scripts/build.bat
+++ scripts/build.bat
@@ -4,5 +4,6 @@ cmake -A x64 -DCMAKE_POSITION_INDEPENDENT_CODE=ON ..
 cmake --build . --config Release
 cmake --build . --config Debug
 
-cp build\src\Debug\jrtplib_d.lib build\src\Debug\jrtplib.lib
-cp build\src\jrtplib-static.dir\Debug\jrtplib-static.pdb build\src\Debug\jrtplib-static.pdb
+
+cp src\Debug\jrtplib_d.lib src\Debug\jrtplib.lib
+cp src\jrtplib-static.dir\Debug\jrtplib-static.pdb src\Debug\jrtplib-static.pdb
