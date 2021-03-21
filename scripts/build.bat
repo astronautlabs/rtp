416d6f2c0377774fb4bbaf6d672818db246348dc
William Lahti <wilahti@gmail.com>
2021-03-21 14:12:35 -0700

    copy lib files to same name to aid in upstream node-gyp builds that do both release and debug

--- scripts/build.bat
+++ scripts/build.bat
@@ -3,3 +3,6 @@ cd build
 cmake -A x64 -DCMAKE_POSITION_INDEPENDENT_CODE=ON ..
 cmake --build . --config Release
 cmake --build . --config Debug
+
+cp build\src\Debug\jrtplib_d.lib build\src\Debug\jrtplib.lib
+cp build\src\jrtplib-static.dir\Debug\jrtplib-static.pdb build\src\Debug\jrtplib-static.pdb
