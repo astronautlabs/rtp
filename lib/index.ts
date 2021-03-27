25df37c778b78c8db89763e152e36cc284c3a603
William Lahti <wilahti@gmail.com>
2021-03-27 16:08:08 -0700

    add TS dirs

--- lib/index.ts
+++ lib/index.ts
@@ -1,1 +1,9 @@
-throw new Error(`This module is (temporarily) only available via C++`);
+import path from 'path';
+
+export const src_include_dir = path.resolve(__dirname, '..', 'src');
+export const build_include_dir = path.resolve(__dirname, '..', 'build', 'src');
+
+export const build_dir = path.resolve(__dirname, '..', 'build');
+
+export const debug_library_dir = path.resolve(__dirname, '..', 'build', 'src', 'Debug');
+export const release_library_dir = path.resolve(__dirname, '..', 'build', 'src', 'Release');
